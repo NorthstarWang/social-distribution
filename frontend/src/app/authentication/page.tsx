66cd07aabba74901a06fdcfeacf97f8be4f16d25
Love Cordelia <1527638985@qq.com>
2024-03-26 14:23:26 -0600

    Added isLoading

--- frontend/src/app/authentication/page.tsx
+++ frontend/src/app/authentication/page.tsx
@@ -29,6 +29,7 @@ export default function AuthenticationPage() {
         } else {
           setIsAuthenticated(false);
           setAuthor(null);
+          setIsLoading(false);
         }
       })
       .catch((error) => {

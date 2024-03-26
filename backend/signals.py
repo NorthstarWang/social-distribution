51ee10de2b08fb1521e90f067a2ff5262dc29b4d
Love Cordelia <1527638985@qq.com>
2024-03-26 13:44:34 -0600

    Reformat

--- backend/signals.py
+++ backend/signals.py
@@ -9,28 +9,12 @@ from .models import Author
 @receiver(user_signed_up)
 def populate_author_profile(sender, **kwargs):
     author = kwargs['user']
-    social_account = SocialAccount.objects.filter(user_id=author.id, provider='github').first()
-
-    if social_account:
-        extra_data = social_account.extra_data
-        author.github = extra_data.get('html_url', '')
-        author.profileImage = extra_data.get('avatar_url', '')
-        author.bio = extra_data.get('bio', '')
-        author.displayName = extra_data.get('name', '')
-        author.save()
+    create_or_update_author(author)
 
 
 @receiver(user_logged_in)
 def update_author_profile_on_login(sender, request, user, **kwargs):
-    social_account = SocialAccount.objects.filter(user_id=user.id, provider='github').first()
-
-    if social_account:
-        extra_data = social_account.extra_data
-        user.github = extra_data.get('html_url', '')
-        user.profileImage = extra_data.get('avatar_url', '')
-        user.bio = extra_data.get('bio', '')
-        user.displayName = extra_data.get('name', '')
-        user.save()
+    create_or_update_author(user)
 
 
 @receiver(social_account_updated)
@@ -40,9 +24,20 @@ def update_author_profile(sender, **kwargs):
 
     if social_account and social_account.user:
         author = social_account.user
-        extra_data = social_account.extra_data
-        author.github = extra_data.get('html_url', '')
-        author.profileImage = extra_data.get('avatar_url', '')
-        author.bio = extra_data.get('bio', '')
-        author.displayName = extra_data.get('name', '')
-        author.save()
+        save_author(author, social_account)
+
+
+def create_or_update_author(author):
+    social_account = SocialAccount.objects.filter(user_id=author.id, provider='github').first()
+
+    if social_account:
+        save_author(author, social_account)
+
+
+def save_author(author, social_account):
+    extra_data = social_account.extra_data
+    author.github = extra_data.get('html_url', '')
+    author.profileImage = extra_data.get('avatar_url', '')
+    author.bio = extra_data.get('bio', '')
+    author.displayName = extra_data.get('name', '')
+    author.save()
